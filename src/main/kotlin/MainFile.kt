263a3d7edb7b48cee51bcc7a3cf0448118b72349
AdhamNa <adhambassiouny@yahoo.com>
2022-08-02 19:29:51 -1200

    version 3

--- src/main/kotlin/MainFile.kt
+++ src/main/kotlin/MainFile.kt
@@ -1,8 +1,10 @@
-// This is version 2
+// This is version 3
 fun main() {
-    val No1:Float = 26.0f
-    val No2:Byte = 20
-    val result = No1+No2
+    var number1:Double = 50.5
+    // number updated
+    number1=20.0
+    val number2:Int =200
+    val result=number1+number2
     println(result)
 
 }
